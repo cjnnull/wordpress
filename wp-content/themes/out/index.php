dbb09dacb06ece68e7279cc279d223278ecb3bc1
cjnnull <792979097@qq.com>
2018-04-04 17:51:29 +0800

    修改

--- wp-content/themes/out/index.php
+++ wp-content/themes/out/index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo get_bloginfo('charset'); ?>" />
-	<title><?php echo bloginfo('name'); ?></title>
+	<title><? bloginfo('name'); ?></title>
 	<meta name="description" content="<?php bloginfo('description'); ?>" />
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
 
@@ -12,14 +12,30 @@
 		<div id="headerimg">
 			<h1><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a></h1>
 			<div class="description"><?php bloginfo('description'); ?></div>
-			<?php
-				$view = get_option('view');
-				update_option( 'view' , $view + 1 );
-			?>
-			<p>访问量：<?php echo $view; ?></p>
 		</div>
 	</div>
-
-	<? wp_footer(); ?>
+	
+	<div id="home-loop">
+		<?php
+			global $post;
+			if( have_posts() ){
+				while( have_posts() ){
+					
+					//获取下一篇文章的信息，并且将信息存入全局变量 $post 中
+					the_post();
+					var_dump($post);
+					?>
+					<div class="post-item">
+						<div class="post-title"><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><h2></div>
+						<div class="post-content"><?php the_content(); ?></div>
+					</div>
+					<?php
+				}
+			}else{
+				echo '没有日志可以显示';
+			}
+		?>
+	</div>
+	<?php wp_footer(); ?>
 </body>
 </html>
